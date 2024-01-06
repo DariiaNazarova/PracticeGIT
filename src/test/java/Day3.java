77f6335ce9055d0b78432830acaa995cc4a7580f
FIRST_NAME LAST_NAME <enigma@MacBook-Air-Daria.local>
2024-01-05 19:43:49 -0600

    january 5th updates

--- src/test/java/Day3.java
+++ src/test/java/Day3.java
@@ -1,5 +1,7 @@
 public class Day3 {
     public static void main(String[] args) {
         System.out.println("This is version 1");
+        System.out.println("Second version");
+        System.out.println("3rd new version");
     }
 }
